d4dea4efed24fac9c4e6ed6cbbfe5abba2e4e2f4
chathurikaPerera <pererachathurika96@gmail.com>
2020-12-02 19:28:40 +0530

    add news categories

---
+++ lib/helper/data.dart
@@ -0,0 +1,41 @@
+import 'package:news_app/model/category_model.dart';
+
+List<CategoryModel> getCategories(){
+
+  List<CategoryModel> category = new List<CategoryModel>();
+  CategoryModel categoryModel = new CategoryModel();
+
+  //1
+  categoryModel.categoryName = "Bussiness";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80";
+  category.add(categoryModel);
+
+  //2
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Entertainment";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  //3
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Health";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  //4
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Foods";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1558467523-46113f1fcf72?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80 ";
+  category.add(categoryModel);
+
+  //5
+  categoryModel =new CategoryModel();
+  categoryModel.categoryName = "Sports";
+  categoryModel.imageUrl =  "https://images.unsplash.com/photo-1517649763962-0c623066013b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+  category.add(categoryModel);
+
+  return category;
+
+
+
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/helper/data.dart';
+import 'package:news_app/model/category_model.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Flutter"),
+            Text("News", style: TextStyle(
+              color: Colors.yellowAccent,
+            ),)
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 70,
+              child: ListView.builder(
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return CategoryTile(
+                    imageUrl: categories[index].imageUrl,
+                    name: categories[index].categoryName  ,
+
+                  );
+                },
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, name;
+  CategoryTile({this.imageUrl, this.name});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+         children: <Widget>[
+           Image.network(imageUrl, width: 120,height: 60,),
+         ],
+      ),
+    );
+  }
+}
+
+
