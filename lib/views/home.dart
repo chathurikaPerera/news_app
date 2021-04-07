1b282e0d240ace18770bb3d0029e1de4f55d9430
chathurikaPerera <pererachathurika96@gmail.com>
2021-04-07 23:39:09 +0530

    2020-12-5

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/helper/data.dart';
+import 'package:news_app/helper/news.dart';
+import 'package:news_app/model/article_model.dart';
 import 'package:news_app/model/category_model.dart';
 
 class Home extends StatefulWidget {
@@ -11,14 +13,24 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   List<CategoryModel> categories = new List<CategoryModel>();
+  List<ArticleModel> articles = new List<ArticleModel>();
+
+  bool _loading = true;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     categories = getCategories();
-
-
   }
+  getNews() async {
+    News newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +47,15 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(
+      body: _loading ? Center(
+        child: Container(
+          child: CircularProgressIndicator(),
+        ),
+      ) : Container(
         child: Column(
           children: <Widget>[
+
+            ///categories
             Container(
               padding: EdgeInsets.symmetric(horizontal: 16),
               height: 70,
@@ -53,8 +71,15 @@ class _HomeState extends State<Home> {
                   );
                 },
               ),
-            )
-
+            ),
+              ///blogs
+            Container(
+              child: ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, index){
+                   return BlogTile(imageUrl: null, title: null, desc: null)
+                }),
+              ),
           ],
         ),
       ),
@@ -99,4 +124,20 @@ class CategoryTile extends StatelessWidget {
   }
 }
 
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+  BlogTile({@required this.imageUrl, @required this.title, @required this.desc})
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget> [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc)
+        ],
+      )
+    );
+  }
+}
 
